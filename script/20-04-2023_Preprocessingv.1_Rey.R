996b89122a37768da62a039934ce70dd1603d980
GashMod-Rey <reynard.yaputra@student.umn.ac.id>
2023-05-20 12:47:29 +0700

    [20-05-2023][Rey] Script DOID Unique, R Script for Find Similarity Matrix, and Script Community Detection

---
+++ script/20-04-2023_Preprocessingv.1_Rey.R
@@ -0,0 +1,24 @@
+# Manual of DO processing
+browseVignettes("GOSemSim")
+rm(list = ls())
+
+# Import dataset
+library(readxl)
+datasheet <- read_excel("./../data/1684561103-DOID-unique-1006.xlsx")
+
+# Preprocessing DOID
+library(DOSE)
+a <- datasheet$`DOID`
+a <- a[!is.na(a)]
+a <- a[! a %in% c('-')]
+a
+
+# Use doSim function to measure similarity
+s <- doSim(a, a, measure = "Wang")
+s <- data.frame(s)
+
+# Write the result to Excel format
+library(openxlsx)
+
+# For writing a data.frame or list of data.frames to an xlsx file
+write.xlsx(s, './../data/adjmatrix.xlsx')
